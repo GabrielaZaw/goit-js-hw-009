d2f724ac1689bfa087699a4d7c22913d07003de6
Gabriela Zawarus <008gz008@gmail.com>
2024-08-09 22:23:06 +0200

    add function color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+startButton.addEventListener('click', () => {
+  if (intervalId) return;
+
+  // Zablokuj przycisk Start, aby nie można było go wielokrotnie klikać
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  // Zmień kolor tła co sekundę
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  // Zatrzymaj zmianę koloru tła
+  clearInterval(intervalId);
+  intervalId = null;
+
+  // Odblokuj przycisk Start
+  startButton.disabled = false;
+  stopButton.diasbled = true;
+});
